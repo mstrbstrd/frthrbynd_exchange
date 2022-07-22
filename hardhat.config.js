57d599ceaec0bfa7e17a6056dbd288e4e20654cd
mstrbstrd <shae@mstrbstrd.com>
2022-07-22 07:21:23 -0700

    2. Smoke Test

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-waffle"); --don't use this
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    localhost: {}
+  },
+};
