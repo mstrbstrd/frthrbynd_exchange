dc3d339c9f251741eb0dc24e38986799dfe92c59
mstrbstrd <shae@mstrbstrd.com>
2022-08-22 14:50:24 -0700

    Mumbai Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,20 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 const privateKeys = process.env.PRIVATE_KEYS.split(',') || ""
-const APIKey = process.env.INFURA_API_KEY || ""
+const INFURA_APIKey = process.env.INFURA_API_KEY || ""
+const ALCHEMY_APIKey = process.env.ALCHEMY_API_KEY || ""
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost: {},
     kovan: {
-      url: `https://kovan.infura.io/v3/${APIKey}`,
+      url: `https://kovan.infura.io/v3/${INFURA_APIKey}`,
       accounts: privateKeys,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_APIKey}`,
+      accounts: privateKeys
     }
   },
 };
