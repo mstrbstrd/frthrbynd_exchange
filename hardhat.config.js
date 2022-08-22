36ca1fd5855ee62e59ff2dcf93b852765ddaa94b
mstrbstrd <shae@mstrbstrd.com>
2022-08-22 13:53:03 -0700

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,18 @@ require("@nomicfoundation/hardhat-toolbox");
 // require("@nomicfoundation/hardhat-waffle"); --don't use this
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
+
+const privateKeys = process.env.PRIVATE_KEYS.split(',') || ""
+const APIKey = process.env.INFURA_API_KEY || ""
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    kovan: {
+      url: `https://kovan.infura.io/v3/${APIKey}`,
+      accounts: privateKeys,
+    }
   },
 };
+
